e64ef85b47a19024d85ab74eca28a4c585b88e08
Aviral Dasgupta <me@aviraldg.com>
2016-04-26 09:43:54 +0530

    various improvements

---
+++ static/app.js
@@ -0,0 +1,20 @@
+(function () {
+    'use strict';
+
+    function processInternalLinks() {
+        var elements = document.querySelectorAll('[id]');
+        for(var i = 0; i < elements.length; i++) {
+            var elem = elements[i];
+            var link = document.createElement("a");
+            link.href = "#" + elem.id;
+            elem.parentNode.replaceChild(link, elem);
+            link.appendChild(elem);
+        }
+    }
+
+    window.addEventListener('load', function () {
+        processInternalLinks();
+    });
+})();
+
+
